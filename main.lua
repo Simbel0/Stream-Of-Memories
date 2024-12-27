41821a10cd44fcb400256fffc4a82e350ec58a1b
Simbel0 <simbel.user@gmail.com>
2024-12-27 23:31:49 +0100

    Stopping the game is nice

--- main.lua
+++ main.lua
@@ -25,5 +25,13 @@ function love.update(dt)
 	Timer.update(dt)
 end
 
+function love.keypressed(key, scancode, is_repeat)
+	if scancode == "escape" then
+		love.event.quit()
+	elseif love.keyboard.isScancodeDown("lctrl") and scancode == "r" then
+		love.event.quit("restart")
+	end
+end
+
 function love.draw()
 end
