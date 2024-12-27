cebeb15ecb5890a5c7859d776fa5fef1eff1bc01
Simbel0 <simbel.user@gmail.com>
2024-12-27 22:15:29 +0100

    Conf file

---
+++ conf.lua
@@ -0,0 +1,14 @@
+function love.conf(t)
+	t.identity = "neurogame"
+
+	t.version = "11.0"
+
+	t.window.title = "Neuro Game"
+
+	-- Undertale real
+	t.window.width = 640
+    t.window.height = 480
+
+    -- Thinking about phones I guess
+    t.externalstorage = true
+end
