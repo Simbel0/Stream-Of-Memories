e965773f9f4ce3b02a0daceef8b62af22ed8fc26
Simbel0 <simbel.user@gmail.com>
2024-12-28 03:24:14 +0100

    Debug stuff like FPS

--- main.lua
+++ main.lua
@@ -24,6 +24,8 @@ function love.load()
 	require("src.vars")
 	print("GAME START: Neuro Game is started!")
 
+	main_font = love.graphics.newFont("assets/fonts/coffee.ttf", 32)
+
 	Timer.after(2, function() print("yay") end)
 
 	GameStateManager.registerEvents()
@@ -32,6 +34,25 @@ end
 
 function love.update(dt)
 	Timer.update(dt)
+	DT = dt
+
+	FPS_TIMER = FPS_TIMER + DT
+	FPS_COUNTER = FPS_COUNTER + 1
+	if FPS_TIMER >= 1 then
+		FPS = FPS_COUNTER
+		FPS_COUNTER = 0
+		FPS_TIMER = FPS_TIMER - 1
+	end
+end
+
+function pressedCtrl()
+	return love.keyboard.isScancodeDown("lctrl") or love.keyboard.isScancodeDown("rctrl")
+end
+function pressedShift()
+	return love.keyboard.isScancodeDown("lshift") or love.keyboard.isScancodeDown("rshift")
+end
+function pressedAlt()
+	return love.keyboard.isScancodeDown("lalt") or love.keyboard.isScancodeDown("ralt")
 end
 
 function love.keypressed(key, scancode, is_repeat)
@@ -39,8 +60,15 @@ function love.keypressed(key, scancode, is_repeat)
 		love.event.quit()
 	elseif love.keyboard.isScancodeDown("lctrl") and scancode == "r" then
 		love.event.quit("restart")
+	elseif pressedCtrl() and scancode == "d" then
+		DEBUG_VIEW = not DEBUG_VIEW
 	end
 end
 
 function love.draw()
+	love.graphics.setFont(main_font)
+	if DEBUG_VIEW then
+		love.graphics.setColor(1, 1, 1, 1)
+		love.graphics.print(FPS.." FPS", 0, 0)
+	end
 end
--- src/vars.lua
+++ src/vars.lua
@@ -1,1 +1,9 @@
 SCREEN_WIDTH, SCREEN_HEIGHT = love.graphics.getDimensions()
+
+DEBUG_VIEW = true
+
+DT = 1/30
+
+FPS = 0
+FPS_TIMER = 0
+FPS_COUNTER = 0
