22bb25e995c306b9e390c36b412111bc416e5d8d
Simbel0 <simbel.user@gmail.com>
2024-12-30 15:59:33 +0100

    Whoops

--- src/states/gameOver.lua
+++ src/states/gameOver.lua
@@ -51,7 +51,7 @@ function gameOver:enter(game)
 > FAILURE.
 > Someone tell Vedal there is a problem with 
   his AI.]]
-  	self.skip_text = self.skip_text .. "\n\n> Error Score: "..game.score
+  	self.skip_text = self.skip_text .. "\n\n> Score: "..game.score
   	self.skip_text = self.skip_text .. "\n> Best Score: "..game.best_score
 end
 
