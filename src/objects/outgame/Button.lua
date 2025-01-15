1372d793e42db470aa434b99f0f29054385a174d
Simbel0 <simbel.user@gmail.com>
2025-01-15 01:17:24 +0100

    Finished the button drawing I think? I hope?? Please tell me it works

--- src/objects/outgame/Button.lua
+++ src/objects/outgame/Button.lua
@@ -71,11 +71,28 @@ function Button:getColor()
 	return r, g, b, self.alpha
 end
 
+function Button:getSecondColor()
+	local r, g, b, a = self:getColor()
+	local light_value = 0.2
+	return r+light_value, g+light_value, b+light_value, a
+end
+
 function Button:draw()
 	love.graphics.setColor(self:getColor())
 	love.graphics.setLineWidth(5)
 
 	love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+
+	love.graphics.setColor(self:getSecondColor())
+	love.graphics.polygon("fill", 
+		self.x, self.y+self.height,
+		self.x+self.width, self.y+self.height,
+		self.x+self.width-self.width/4, self.y,
+		self.x+self.width/4, self.y
+	)
+
+	love.graphics.setColor(1, 1, 1, self.alpha)
+	love.graphics.printf(self.title, self.x, self.y+(self.height/2)-16, self.width, "center")
 
 	local corner_width, corner_height = BUTTON_CORNER_TEXTURE:getDimensions()
 
@@ -90,19 +107,19 @@ function Button:draw()
 	love.graphics.line(x, y, x+(self.width-corner_width), y)
 
 	-- top-right
-	love.graphics.draw(BUTTON_CORNER_TEXTURE, x+self.width, self.y-corner_height/2, math.rad(90))
+	love.graphics.draw(BUTTON_CORNER_TEXTURE, x+self.width, (self.y-corner_height/2)+1, math.rad(90))
 
 	local x2, y2 = x+(self.width-corner_width/2)+1.5, self.y+corner_height-5
 	love.graphics.line(x2, y2, x2, y2+(self.height-corner_height))
 
 	-- bottom-right
-	love.graphics.draw(BUTTON_CORNER_TEXTURE, (x2+corner_width/2)-3, y2+(self.height), math.rad(90*2))
+	love.graphics.draw(BUTTON_CORNER_TEXTURE, (x2+corner_width/2)-3, (y2+(self.height))-1, math.rad(90*2))
 
 	local x3, y3 = self.x+self.width-corner_width/2, self.y+self.height+2
 	love.graphics.line(x3, y3, self.x+corner_width/2, y3)
 
 	-- bottom-left
-	love.graphics.draw(BUTTON_CORNER_TEXTURE, self.x-corner_width/2, (self.y+self.height)+corner_height/2, math.rad(90*3))
+	love.graphics.draw(BUTTON_CORNER_TEXTURE, (self.x-corner_width/2)-0.4, ((self.y+self.height)+corner_height/2)-1, math.rad(90*3))
 
 	love.graphics.line((self.x-corner_width/2)+3.5, (self.y+self.height)-corner_height/2, (self.x-corner_width/2)+3.5, self.y+corner_height/2)
 end
